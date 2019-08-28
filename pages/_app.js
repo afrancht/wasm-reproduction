9251eeeaee735a2701174c43f623d17c7fbb02a7
Alex FT <franchtapia@gmail.com>
2019-08-28 14:24:48 +0200

    deleted non-ncessary script in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,6 @@ class MyApp extends App {
         <Helmet>
           <title>Privasee</title>
           <meta name="description" content="Dashboard Page" />
-          <script src="node_modules/argon2-browser/lib/argon2.js"></script>
           <link
             rel="stylesheet"
             href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
